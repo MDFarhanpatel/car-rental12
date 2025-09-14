0c6c033ad50a2a165cfae0f44140ab8d26507e0a
farhanpatel <mohammedfarhanpatel786@gmail.com>
2025-09-14 23:33:44 +0530

    updated code

--- src/app/pages/users/page.js
+++ src/app/pages/users/page.js
@@ -352,7 +352,7 @@ export default function UsersPage() {
             icon: "pi pi-home",
             command: () => (window.location.href = "/"),
           }}
-          className="text-white font-bold mb-2"
+          className="text-white font-bold mb-2 sm:mb-4"
         />
       </div>
       <div className="text-2xl sm:text-3xl font-extrabold text-white mb-2 sm:mb-3 tracking-wide">
@@ -387,7 +387,6 @@ export default function UsersPage() {
           loading={loading}
           className="p-datatable-sm text-white"
           emptyMessage="No users found."
-          responsiveLayout="scroll"
         >
           <Column
             field="name"
@@ -642,6 +641,16 @@ export default function UsersPage() {
             disabled={editSaveDisabled}
           />
           <Button
+            label="Cancel"
+            className="bg-gray-400 border-none font-extrabold px-4 sm:px-6 py-2 text-sm sm:text-base rounded-lg"
+            onClick={() => setShowEdit(false)}
+          />
+        </div>
+      </Dialog>
+    </div>
+  );
+}
+   <Button
             label="Cancel"
             className="bg-gray-400 border-none font-extrabold px-4 sm:px-6 py-2 text-sm sm:text-base rounded-lg"
             onClick={() => setShowEdit(false)}
